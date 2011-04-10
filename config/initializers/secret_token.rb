53bd3be7f5771d6c5858e3e4bbe10e4ccc2d13e4
John Richardson <jrichardson@ncsasports.org>
2011-04-10 13:48:23 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '154dc7a92ccd197296e64c4d5333815a2596407c47066d9d87bd1c6cdd23135bca854bd5e546974d319eddf65f8483c1373566ba52357ff4023a91910c397aac'
